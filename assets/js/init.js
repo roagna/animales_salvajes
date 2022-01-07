5041afd17033ff2823cb3f2284833ef6e2672794
Giglia Roagna <gigliaroagna@Giglias-MBP.home>
2022-01-06 22:25:58 -0300

    mi primer commit

---
+++ assets/js/init.js
@@ -0,0 +1,19 @@
+(async function () {
+  // voy a buscar los datos del json
+  let data = await fetch("data/animales.json");
+  // los desempaqueto
+  const data2 = await data.json();
+  const animales = data2.animales;
+
+  $("#animal").on("change", function () {
+    // obtengo el valor del "option" seleccionado
+    const nombre_animal = $("#animal").val();
+
+    const animal = animales.find((an) => an.name == nombre_animal);
+
+    $("#preview").css(
+      "background-image",
+      `url("assets/imgs/${animal.imagen}")`
+    );
+  });
+})(); // IIFE (Funciones Inmediatamente Ejecutadas)
